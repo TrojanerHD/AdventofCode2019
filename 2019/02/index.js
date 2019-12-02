795c9f9c3d396bb44dc65f55db4adbe59d43e3be
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2019-12-02 20:46:12 +0100

    Added 2019/02 Part 2

--- 2019/02/index.js
+++ 2019/02/index.js
@@ -9,29 +9,54 @@ function main () {
       return
     }
 
-    const array = data.split(',')
-    array[1] = 12
-    array[2] = 2
+    //Part 1
+    let array = data.split(',')
+    array[1] = '12'
+    array[2] = '2'
+    console.log(`[Day 2] Part 1: The first element's value is ${parseIntCode(array)}`)
 
-    let count = 0
-    for (const opcode of array) {
-      if (count % 4 === 0) {
-        const firstElement = parseInt(array[count + 1])
-        const secondElement = parseInt(array[count + 2])
-        const overrideElement = parseInt(array[count + 3])
-        switch (parseInt(opcode)) {
-          case 1:
-            array[overrideElement] = (parseInt(array[firstElement]) + parseInt(array[secondElement])).toString()
-            break
-          case 2:
-            array[overrideElement] = (parseInt(array[firstElement]) * parseInt(array[secondElement])).toString()
-            break
-          case 99:
-            console.log(`[Day 2] The first element's value is ${array[0]}`)
-            return
-        }
+    //Part 2
+    array = data.split(',')
+
+    let firstElementCounter = 0
+    let secondElementCounter = 0
+    while (firstElementCounter <= 99 && secondElementCounter <= 99) {
+      array[1] = firstElementCounter.toString()
+      array[2] = secondElementCounter.toString()
+      if (parseInt(parseIntCode(array)) === 19690720) console.log(`[Day 2] Part 2: Calculated answer (100 * noun + verb): ${100 * firstElementCounter + secondElementCounter}`)
+
+      array = data.split(',')
+      if (firstElementCounter < 99) firstElementCounter++
+      else {
+        firstElementCounter = 0
+        secondElementCounter++
       }
-      count++
+
     }
   })
+}
+
+function parseIntCode (array) {
+  let count = 0
+  for (const opcode of array) {
+    if (count % 4 === 0) {
+      const firstElement = parseInt(array[count + 1])
+      const secondElement = parseInt(array[count + 2])
+      const overrideElement = parseInt(array[count + 3])
+      let done = false
+      switch (parseInt(opcode)) {
+        case 1:
+          array[overrideElement] = (parseInt(array[firstElement]) + parseInt(array[secondElement])).toString()
+          break
+        case 2:
+          array[overrideElement] = (parseInt(array[firstElement]) * parseInt(array[secondElement])).toString()
+          break
+        case 99:
+          done = true
+          break
+      }
+      if (done) return array[0]
+    }
+    count++
+  }
 }
