bad3b25ba427ecdc10641409212eeb0fe7a8a5c6
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2019-12-02 14:15:29 +0100

    Changed structure: You can now call executeDay(year, day) in ./index.js to see the results for the given year and day

--- 2019/01/index.js
+++ 2019/01/index.js
@@ -1,28 +1,30 @@
 const fs = require('fs')
 
-fs.readFile('./values.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
+module.exports = main
 
-  const values = data.split('\n')
-
-  //Total calculation for first half of the puzzle
-  let total = 0
-  let totalWithFuelForFuel = 0
-  for (let value of values) {
-    let fuel = Math.floor(parseInt(value) / 3) - 2
-    total += fuel
-    while (fuel > 0) {
-      const oldFuel = fuel
-      fuel = Math.floor(fuel / 3) - 2
-      totalWithFuelForFuel += oldFuel
+function main () {
+  fs.readFile('./values.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
     }
-  }
 
-  console.log(`Total fuel (part 1 of the puzzle): ${total}`)
-  console.log(`Total fuel with fuel for fuel etc. (part 2 of the puzzle): ${totalWithFuelForFuel}`)
+    const values = data.split('\n')
 
-  //
-})
+    //Total calculation for first half of the puzzle
+    let total = 0
+    let totalWithFuelForFuel = 0
+    for (let value of values) {
+      let fuel = Math.floor(parseInt(value) / 3) - 2
+      total += fuel
+      while (fuel > 0) {
+        const oldFuel = fuel
+        fuel = Math.floor(fuel / 3) - 2
+        totalWithFuelForFuel += oldFuel
+      }
+    }
+
+    console.log(`Total fuel (part 1 of the puzzle): ${total}`)
+    console.log(`Total fuel with fuel for fuel etc. (part 2 of the puzzle): ${totalWithFuelForFuel}`)
+  })
+}
