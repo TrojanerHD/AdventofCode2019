8fdaa72f6ff3711735b5c1de5bc542c500e22353
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2019-12-04 15:15:40 +0100

    Added unfinished solution for 2019/04 Part 1

---
+++ 2019/04/index.js
@@ -0,0 +1,24 @@
+module.exports = main
+
+function main () {
+  let currentInput = 145852
+  const maxInput = 616942
+  let solutionNumber = 0
+  while (currentInput <= maxInput) {
+    let maxValue = 0
+    let skip = false
+    let notSkip = false
+    for (const digit of currentInput.toString().split('')) {
+      if (parseInt(digit) > maxInput) {
+        skip = true
+        break
+      }
+      if (!notSkip && parseInt(digit) === maxValue) notSkip = true
+      maxValue = parseInt(digit)
+    }
+    currentInput++
+    if (skip || !notSkip) continue
+    solutionNumber++
+  }
+  console.log(`[Day 4] The number of possible solutions is ${solutionNumber}`)
+}
