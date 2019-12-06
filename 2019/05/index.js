c64ba5ca504517abb8ccee6b6a97827e82d3db11
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2019-12-06 22:47:05 +0100

    Added solution for 2019/05 Part 2

--- 2019/05/index.js
+++ 2019/05/index.js
@@ -14,43 +14,53 @@ function main () {
 }
 
 function parseIntCode (array) {
-  let count = 0
-  let increase = 0
-  for (let opcode of array) {
-    if (increase > 0) {
-      increase--
-      count++
-      continue
-    }
-
+  for (let i = 0; i < array.length; i++) {
+    let opcode = array[i]
     while (opcode.split('').length < 5) opcode = `0${opcode}`
     const parameterMode = opcode.split('')
-    const firstElement = parseInt(array[count + 1])
-    const secondElement = parseInt(array[count + 2])
-    const overrideElement = parseInt(array[count + 3])
+    const firstParameter = parseInt(array[i + 1])
+    const secondParameter = parseInt(array[i + 2])
+    const overrideParameter = parseInt(array[i + 3])
     let done = false
     switch (parseInt(opcode.substr(opcode.length - 2, opcode.length - 1))) {
       case 1:
-        array[overrideElement] = ((parameterMode[2] === '0' ? parseInt(array[firstElement]) : firstElement) + (parameterMode[1] === '0' ? parseInt(array[secondElement]) : secondElement)).toString()
-        increase = 3
+        array[overrideParameter] = ((parameterMode[2] === '0' ? parseInt(array[firstParameter]) : firstParameter) + (parameterMode[1] === '0' ? parseInt(array[secondParameter]) : secondParameter)).toString()
+        i += 3
         break
       case 2:
-        array[overrideElement] = ((parameterMode[2] === '0' ? parseInt(array[firstElement]) : firstElement) * (parameterMode[1] === '0' ? parseInt(array[secondElement]) : secondElement)).toString()
-        increase = 3
+        array[overrideParameter] = ((parameterMode[2] === '0' ? parseInt(array[firstParameter]) : firstParameter) * (parameterMode[1] === '0' ? parseInt(array[secondParameter]) : secondParameter)).toString()
+        i += 3
         break
       case 3:
-        array[firstElement] = '1'
-        increase = 1
+        array[firstParameter] = '5'
+        i += 1
         break
       case 4:
-        console.log(parameterMode[2] === '0' ? array[firstElement] : firstElement)
-        increase = 1
+        console.log(parameterMode[2] === '0' ? array[firstParameter] : firstParameter)
+        i += 1
+        break
+      case 5:
+        if ((parameterMode[2] === '0' ? parseInt(array[firstParameter]) : firstParameter) !== 0) i = (parameterMode[1] === '0' ? parseInt(array[secondParameter]) : secondParameter) - 1
+        else i += 2
+        break
+      case 6:
+        if ((parameterMode[2] === '0' ? parseInt(array[firstParameter]) : firstParameter) === 0) i = (parameterMode[1] === '0' ? parseInt(array[secondParameter]) : secondParameter) - 1
+        else i += 2
+        break
+      case 7:
+        if ((parameterMode[2] === '0' ? parseInt(array[firstParameter]) : firstParameter) < (parameterMode[1] === '0' ? parseInt(array[secondParameter]) : secondParameter)) array[overrideParameter] = '1'
+        else array[overrideParameter] = '0'
+        i += 3
+        break
+      case 8:
+        if ((parameterMode[2] === '0' ? parseInt(array[firstParameter]) : firstParameter) === (parameterMode[1] === '0' ? parseInt(array[secondParameter]) : secondParameter)) array[overrideParameter] = '1'
+        else array[overrideParameter] = '0'
+        i += 3
         break
       case 99:
         done = true
         break
     }
     if (done) return array[0]
-    count++
   }
 }
