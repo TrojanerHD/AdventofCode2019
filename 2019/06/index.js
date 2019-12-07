9cae0963959d450898e1aaa3a929c9d462223daf
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2019-12-07 13:46:55 +0100

    Added 2019/06 Part 2

--- 2019/06/index.js
+++ 2019/06/index.js
@@ -17,8 +17,10 @@ function main () {
       results[orbitSplit[0]].push(orbitSplit[1])
     }
 
+    let san,
+      you
+    let result
     for (const key of Object.keys(results)) {
-      const valueArray = results[key]
       let skip = false
       for (const value of Object.values(results)) {
         if (value.includes(key)) {
@@ -26,9 +28,25 @@ function main () {
           break
         }
       }
+      if (results[key].includes('SAN')) {
+        san = traceBack(results, key, 'COM')
+      }
+      if (results[key].includes('YOU')) {
+        you = traceBack(results, key, 'COM')
+      }
+      let total = []
+      if (you && san) {
+        const youSplit = you.values.split(','),
+          sanSplit = san.values.split(',')
+        total.push(youSplit.filter(element => sanSplit.includes(element)))
+        result = ((you.values.split(total[0][0])[0].match(/,/g) || []).length) + ((san.values.split(total[0][0])[0].match(/,/g) || []).length)
+        you = undefined
+        san = undefined
+      }
       if (skip) continue
-      const count = getOrbits(results, key, 0)
-      console.log(count)
+      const orbits = getOrbits(results, key, 0)
+      console.log(`[Day 6] Part 1: The number of indirect and direct orbits is ${orbits}`)
+      console.log(`[Day 6] Part 2: At least ${result} steps are required to get from YOU to SAN`)
     }
   })
 }
@@ -43,4 +61,22 @@ function getOrbits (results, key, indentation) {
     count += getOrbits(results, value, indentation)
   }
   return count
+}
+
+function traceBack (results, value, ending) {
+  let values = ''
+  let count = 0
+  for (const key of Object.keys(results)) {
+    if (results[key].includes(value)) {
+      count++
+      if (key === ending) {
+        values += `${value},${key}`
+        break
+      }
+      const trace = traceBack(results, key, ending)
+      values += `${value},${trace.values}`
+      count += trace.count
+    }
+  }
+  return { values, count }
 }
