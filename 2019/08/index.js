fa844e2bbe234bd00635ddfeb1e3a8e5d30abdf6
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2019-12-08 17:18:21 +0100

    Added 2019/08 Part 2

--- 2019/08/index.js
+++ 2019/08/index.js
@@ -21,5 +21,28 @@ function main () {
       two: (zeroCount.layer.match(/2/g) || []).length
     }
     console.log(`[Day 8] Part 1: The number of 1 digits multiplied by the number of 2 digits is ${numbersInLayer.one * numbersInLayer.two}`)
+
+    let resultObject = {}
+    for (const layer of layers) {
+      let count = 0
+      const linebreaks = layer.match(/.{1,25}/g)
+      for (const linebreak of linebreaks) {
+        const linebreakChars = linebreak.split('')
+        for (const linebreakChar of linebreakChars) {
+          if (!resultObject[count] || resultObject[count] === '2')
+            resultObject[count] = linebreakChar
+          count++
+        }
+        resultObject[count] = '\n'
+        count++
+      }
+    }
+    let result = ''
+    for (const value of Object.values(resultObject)) {
+      result += value
+    }
+    result = result.replace(/0/g, ' ')
+    result = result.replace(/1/g, '☐')
+    console.log(`[Day 8] Part 2:\n${result}-------------`)
   })
 }
